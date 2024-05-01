bc1e8b982dc89eb99a3067868aca50177482becc
Tommy J Burke <thatgingerdude@gmail.com>
2024-05-01 23:29:08 +0100

    tidy-up

--- src/App.jsx
+++ src/App.jsx
@@ -124,6 +124,50 @@ function WatchedBox() {
 
 function WatchedList() {
    const [watched, setWatched] = useState(tempWatchedData)
+
+   return (
+      <>
+         <WatchedSummary watched={watched} />
+
+         <WatchedMovieList watched={watched} />
+      </>
+   )
+}
+
+function WatchedMovieList({ watched }) {
+   return (
+      <ul className='list'>
+         {watched.map((movie) => (
+            <WatchedMovie movie={movie} key={movie.imdbID} />
+         ))}
+      </ul>
+   )
+}
+
+function WatchedMovie({ movie }) {
+   return (
+      <li>
+         <img src={movie.Poster} alt={`${movie.Title} poster`} />
+         <h3>{movie.Title}</h3>
+         <div>
+            <p>
+               <span>⭐️</span>
+               <span>{movie.imdbRating}</span>
+            </p>
+            <p>
+               <span>🌟</span>
+               <span>{movie.userRating}</span>
+            </p>
+            <p>
+               <span>⏳</span>
+               <span>{movie.runtime} min</span>
+            </p>
+         </div>
+      </li>
+   )
+}
+
+function WatchedSummary({ watched }) {
    const avgImdbRating = average(
       watched.map((movie) => movie.imdbRating)
    )
@@ -134,55 +178,27 @@ function WatchedList() {
       watched.map((movie) => movie.runtime)
    )
    return (
-      <>
-         <div className='summary'>
-            <h2>Movies you watched</h2>
-            <div>
-               <p>
-                  <span>#️⃣</span>
-                  <span>{watched.length} movies</span>
-               </p>
-               <p>
-                  <span>⭐️</span>
-                  <span>{avgImdbRating}</span>
-               </p>
-               <p>
-                  <span>🌟</span>
-                  <span>{avgUserRating}</span>
-               </p>
-               <p>
-                  <span>⏳</span>
-                  <span>{avgRuntime} min</span>
-               </p>
-            </div>
+      <div className='summary'>
+         <h2>Movies you watched</h2>
+         <div>
+            <p>
+               <span>#️⃣</span>
+               <span>{watched.length} movies</span>
+            </p>
+            <p>
+               <span>⭐️</span>
+               <span>{avgImdbRating}</span>
+            </p>
+            <p>
+               <span>🌟</span>
+               <span>{avgUserRating}</span>
+            </p>
+            <p>
+               <span>⏳</span>
+               <span>{avgRuntime} min</span>
+            </p>
          </div>
-
-         <ul className='list'>
-            {watched.map((movie) => (
-               <li key={movie.imdbID}>
-                  <img
-                     src={movie.Poster}
-                     alt={`${movie.Title} poster`}
-                  />
-                  <h3>{movie.Title}</h3>
-                  <div>
-                     <p>
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                     </p>
-                     <p>
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                     </p>
-                     <p>
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                     </p>
-                  </div>
-               </li>
-            ))}
-         </ul>
-      </>
+      </div>
    )
 }
 
